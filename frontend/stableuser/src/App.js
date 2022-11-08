b51b483a9388efbdeeaafc283e6af05452117a28
reka <debreka@gmail.com>
2022-11-08 11:28:01 +0100

    add logo

--- frontend/stableuser/src/App.js
+++ frontend/stableuser/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './pics/horse.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className='main_logo' alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
