573a19303a4807817874493f2d5830ad641dd868
reka <debreka@gmail.com>
2022-11-10 13:56:19 +0100

    add breadcrumb

--- frontend/stableuser/src/Components/App.js
+++ frontend/stableuser/src/Components/App.js
@@ -3,17 +3,20 @@ import '../style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../pics/horse.png';
 import Container from 'react-bootstrap/Container';
+import Header from './Header';
 
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <Container fluid>
           <img src={logo} className='main_logo' alt="logo" />
 
         </Container>
-      </header>
+      
     </div>
   );
 }
--- frontend/stableuser/src/Components/Header.js
+++ frontend/stableuser/src/Components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Breadcrumb>
+    
+      <Breadcrumb.Item >
+      Horses
+      </Breadcrumb.Item>
+      <Breadcrumb.Item >
+      Ferrier
+      </Breadcrumb.Item>
+      <Breadcrumb.Item >
+      Vet
+      </Breadcrumb.Item>
+      <Breadcrumb.Item >
+     Owners
+      </Breadcrumb.Item>
+    </Breadcrumb>
   )
 }
