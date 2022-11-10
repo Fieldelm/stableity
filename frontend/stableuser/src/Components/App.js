5372147f10ad41337249ac03e3c0017aa4042921
reka <debreka@gmail.com>
2022-11-10 21:33:31 +0100

    add language from cookie

--- frontend/stableuser/src/Components/App.js
+++ frontend/stableuser/src/Components/App.js
@@ -4,17 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../pics/horse.png';
 import Container from 'react-bootstrap/Container';
 import Header from './Header';
+import i18next from 'i18next'
+import { useTranslation } from 'react-i18next'
 
 
 function App() {
 
+
+
+const languages = [
+  {
+    code: 'hu',
+    name: 'Magyar',
+    country_code: 'hu',
+  },
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  }
+]
+const { t } = useTranslation();
 
   return (
     <div className="App">
       <Header />
         <Container fluid>
           <img src={logo} className='main_logo' alt="logo" />
-
+          <h1 className="font-weight-normal mb-3">{t('welcome_msg')}</h1>
         </Container>
       
     </div>
--- frontend/stableuser/src/index.js
+++ frontend/stableuser/src/index.js
@@ -1,16 +1,51 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import App from './Components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+//import App from './Components/App';
 //import reportWebVitals from './reportWebVitals';
+
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from 'i18next-http-backend';
+import { useTranslation } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18next
+  
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+
+    // supportedLngs: ['en', 'hu'],
+
+    fallbackLng: 'en',
+    // debug: false,
+    // Options for language detector
+    detection: {
+      order: ['cookie','htmlTag', 'path' ],
+      caches: ['cookie']
+    },
+     backend: {
+     loadPath: './locales/{{lng}}/translation.json',
+   },
+  })
+
+function App() {
+  const { t } = useTranslation();
+  return <h1 className="font-weight-normal mb-3">{t('welcome_msg')}</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-    <App />
-    </Suspense>
-  </React.StrictMode>
+  <Suspense fallback={<div>Loading...</div>}>
+    <React.StrictMode>
+      {/* <App /> */}
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
